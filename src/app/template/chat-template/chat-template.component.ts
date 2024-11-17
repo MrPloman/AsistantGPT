c062e7d5fa5a306c7b2dc76bbbf4ec2f1ef54721
MrPloman <polplanaayet@gmail.com>
2024-11-17 10:39:39 -0600

    finished template of chat-message

--- src/app/template/chat-template/chat-template.component.ts
+++ src/app/template/chat-template/chat-template.component.ts
@@ -5,21 +5,23 @@ import {
   inject,
   signal,
 } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ChatMessageComponent } from '@components/chat-bubbles/chat-message/chat-message.component';
 import { MyMessageComponent } from '@components/chat-bubbles/my-message/my-message.component';
-import { TypingLoaderComponent } from '@components/typing-loader/typing-loader.component';
 import { TextMessageBoxComponent } from '@components/text-message-box/text-message-box.component';
 import { TextMessageFileBoxComponent } from '@components/text-message-file-box/text-message-file-box.component';
 import { TextMessageSelectBoxComponent } from '@components/text-message-select-box/text-message-select-box.component';
+import { TypingLoaderComponent } from '@components/typing-loader/typing-loader.component';
 import { Message } from 'app/interfaces/message.interface';
 import { MessagesMock } from 'app/mocks/messages.mock';
-import { OpenAIService } from '../../services/openAI.service';
+import { OpenAIService } from 'app/presentation/services/openAI.service';
 
 @Component({
-  selector: 'app-ortography-page',
+  selector: 'app-chat-template',
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ChatMessageComponent,
     MyMessageComponent,
     TypingLoaderComponent,
@@ -28,10 +30,10 @@ import { OpenAIService } from '../../services/openAI.service';
     TextMessageSelectBoxComponent,
     TextMessageSelectBoxComponent,
   ],
-  templateUrl: './ortography-page.component.html',
+  templateUrl: './chat-template.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class OrtographyPageComponent {
+export class ChatTemplateComponent {
   public messages = signal<Message[]>(MessagesMock);
 
   public isLoading = signal<boolean>(false);
