c48549ba77dcb0707bf2e99ea2b2db6f425617dc
MrPloman <polplanaayet@gmail.com>
2024-11-19 19:50:29 -0600

    Last change in mock

--- src/app/mocks/messages.mock.ts
+++ src/app/mocks/messages.mock.ts
@@ -2,11 +2,7 @@ import { Message } from 'app/interfaces/message.interface';
 
 export const MessagesMock: Message[] = [
   {
-    text: 'Hola Mundo',
+    text: 'Welcome! Here is the place to check your texts. I am going to check all the syntax and the misspelled words. You only have to write your text and take it easy!',
     isGpt: true,
-  },
-  {
-    text: 'Cual es tu color favorito?',
-    isGpt: false,
   },
 ];
