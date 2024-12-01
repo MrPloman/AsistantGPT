275a1412736c76747235951f7530cd910452d83f
MrPloman <polplanaayet@gmail.com>
2024-12-01 17:27:12 -0600

    commented usecase

--- src/app/core/useCases/pros-cons-stream-discusser-case.ts
+++ src/app/core/useCases/pros-cons-stream-discusser-case.ts
@@ -5,6 +5,7 @@ export async function* prosConsStreamDiscusserUseCase(
   abortSignal: AbortSignal
 ) {
   try {
+    // Response to await, important to send the abortSignal in the req
     const response = await fetch(
       `${environment.BACKEND_URL}/${environment.CASES.PROS_CONS_STREAM_DISCUSSER}`,
       {
@@ -16,19 +17,27 @@ export async function* prosConsStreamDiscusserUseCase(
         signal: abortSignal,
       }
     );
-    if (!response.ok)
+
+    // Control structure to prevent no response
+    if (!response || !response.ok)
       throw new Error('Could not make the pros cons discussion connection...');
 
+    // Get reader of the response. This object is only for streams
     const reader = response.body?.getReader();
-
+    // Preventions when the reader is not gotten.
     if (!reader) throw new Error('Could not get the reader');
 
+    // Create a decoder which allows to translate the stream object to text
     const decoder = new TextDecoder();
+    // Initialize text variable
     let text = '';
 
+    // Infinite bucle which is going to be finished when the stream is ended.
     while (true) {
+      // Get the value of each stream and done value, which indicates if it is already finished
       const { value, done } = await reader.read();
       if (done) break;
+      // Use the decoder to get the text from the stream
       const decodeChunk = decoder.decode(value, { stream: true });
       text += decodeChunk;
       yield text;
